200ec8ba54688f6cbdf7eb169db22149d5da2a20
Marcus V. Ximenes <marcus@marcusvinicius.info>
2025-05-23 16:45:51 -0300

    Initial Commit

---
+++ FocusTimerTests/FocusTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerTests.swift
+//  FocusTimerTests
+//
+//  Created by Marcus Vinicius Ximenes on 23/05/25.
+//
+
+import Testing
+@testable import FocusTimer
+
+struct FocusTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
